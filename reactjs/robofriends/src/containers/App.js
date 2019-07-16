a7800d4781c90b52def4b9762678236882725ed7
aevi <aevirontos@gmail.com>
2019-07-15 21:45:02 -0400

    redux

---
+++ reactjs/robofriends/src/containers/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry'
+import './App.css';
+
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => 
+                this.setState({
+                    robots: users
+                })
+            );
+   
+    }
+
+    onSearchChange = (evt) => {
+
+        this.setState({
+            searchField: evt.target.value
+        });
+
+    }
+
+    render() {
+
+        const { robots, searchField } = this.state;
+
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase());
+        });
+
+        return !robots.length ?
+                <h1>Loading...</h1> :
+                (
+                    <div className='tc'>
+                        <h1 className='f1'>RoboFriends</h1>
+                        <SearchBox searchChange={this.onSearchChange} />
+                        <Scroll>
+                            <ErrorBoundry>
+                                <CardList robots={filteredRobots}/> 
+                            </ErrorBoundry>
+                        </Scroll>
+                    </div>
+                )
+
+    }
+}
+
+export default App;
--- reactjs/robofriends/src/index.js
+++ reactjs/robofriends/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
-import CardList from './CardList'
+import App from './containers/App'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
-import {robots} from './Robots'
 
-
-ReactDOM.render(<CardList robots={robots}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
